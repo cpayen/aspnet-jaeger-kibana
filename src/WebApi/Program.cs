30211b242196d61a81b2aec36ddc6cea076672ae
DESKTOP-LG54DD7\Camille <camille.payen.pro@gmail.com>
2023-08-04 19:44:22 +0200

    add OTLP exporter and Jaeger infra

--- src/WebApi/Program.cs
+++ src/WebApi/Program.cs
@@ -1,3 +1,6 @@
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,20 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Add OpenTelemetry exporter
+builder.Services.AddOpenTelemetry().WithTracing(b => 
+    b.SetResourceBuilder(
+        ResourceBuilder.CreateDefault().AddService(builder.Environment.ApplicationName))
+        .AddAspNetCoreInstrumentation(o => o.Filter = c =>
+        {
+            if (c.Request.Path.HasValue)
+            {
+                return !c.Request.Path.Value.StartsWith("/swagger");
+            }
+            return true;
+        })
+        .AddOtlpExporter(o =>o.Endpoint = new Uri("http://localhost:4317")));
 
 var app = builder.Build();
 
